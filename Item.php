b1331b0a121a4e4757d9496641b77827bbeb3520
Alexlele <vistatsw@icloud.com>
2020-09-08 20:08:01 +0800

    3.0

--- Item.php
+++ Item.php
@@ -52,7 +52,7 @@ $category = $_GET["category"];
 ?>
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand "  href="index.html">E&A</a>
+      <a class="navbar-brand "  href="index.php">E&A</a>
       <a class="navbar-brand" href="#">
         <img src="Logo.png" width="30" height="30" alt="" loading="lazy"/>
       </a>
@@ -63,7 +63,7 @@ $category = $_GET["category"];
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="Home.php">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="Login.html">Login</a>
--- MOBA.php
+++ MOBA.php
@@ -50,7 +50,7 @@ if ($result->num_rows > 0) {
 ?>
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand "  href="index.html">E&A</a>
+      <a class="navbar-brand "  href="index.php">E&A</a>
       <a class="navbar-brand" href="#">
         <img src="Logo.png" width="30" height="30" alt="" loading="lazy"/>
       </a>
@@ -61,7 +61,7 @@ if ($result->num_rows > 0) {
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="Home.php">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="Login.html">Login</a>
